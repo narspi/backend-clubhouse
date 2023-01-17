040f388bbe0b9a6a5670d775e872987065ce6103
narspi <ivanovmichurina@gmail.com>
2023-01-17 09:43:14 +0300

    add data for first time

--- db/core.js
+++ db/core.js
@@ -1,13 +1,37 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('postgres://setner:M4NbcMZH9MmvXrCs4x8tB97rJttDnReW@dpg-cf33hpmn6mpkr6fr67mg-a/clubhouse_9din');
-let message = 'not work';
+const sequelize = new Sequelize(
+  "postgres://setner:M4NbcMZH9MmvXrCs4x8tB97rJttDnReW@dpg-cf33hpmn6mpkr6fr67mg-a/clubhouse_9din"
+);
+let message = "not work";
 
 try {
-  await sequelize.authenticate()
-  message = 'it is ok';
+  await sequelize.authenticate();
+  message = "it is ok";
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
-export { message }
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+User.create({
+  name: "Tom",
+  age: 35,
+});
+
+export { User };
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express";
-import { message } from  './db/core.js';
+import { User } from  './db/core.js';
 
 const app = express();
 
 app.get("/", function(request, response){
-    response.send(`<h2>${message}</h2>`);
+    response.send(`<h2>just add data</h2>`);
 });
 
 
 app.listen(3001, (err) => {
   if (err) {
-    throw new Error("Ошибка какая то");
+    throw new Error("ERROR");
   }
   console.log("server work!");
 });
